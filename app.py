9f3fae0ddaeba1a418d029d367088058733b7768
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-07-26 23:35:40 +0530

    s

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import difflib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+from settings import PORT
 
 app = Flask(__name__)
 CORS(app)
